25f5e0be4604fb6b682e3d55cfead58e8cbb6b4e
Artas Yaskar <artasyaskar@gmail.com>
2025-06-07 02:30:07 +0500

    Update threat_signatures.py

--- core/threat_signatures.py
+++ core/threat_signatures.py
@@ -1,1 +1,6 @@
-
+KNOWN_THREATS = {
+    "SQL Injection": r"(SELECT|INSERT|DELETE|UPDATE).*FROM",
+    "XSS Attempt": r"<script>.*</script>",
+    "Path Traversal": r"\.\.\/",
+    "Suspicious IP": r"192\.168\.1\.\d{1,3}"
+}
