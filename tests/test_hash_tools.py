4f7fb8f045bc969b8460d091701557c55734f2f3
Artas Yaskar <artasyaskar@gmail.com>
2025-06-07 02:36:22 +0500

    Update test_hash_tools.py

--- tests/test_hash_tools.py
+++ tests/test_hash_tools.py
@@ -1,1 +1,10 @@
+import tempfile
+from utils.hash_tools import calculate_sha256
 
+def test_hash_output_consistency():
+    with tempfile.NamedTemporaryFile(delete=False, mode='w') as f:
+        f.write("testdata")
+        path = f.name
+    hash1 = calculate_sha256(path)
+    hash2 = calculate_sha256(path)
+    assert hash1 == hash2
