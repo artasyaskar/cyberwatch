ce1b3edfcf3b443a14dbf9fd5a1f45def5806151
Artas Yaskar <artasyaskar@gmail.com>
2025-06-07 02:33:20 +0500

    Update hash_tools.py

--- utils/hash_tools.py
+++ utils/hash_tools.py
@@ -1,1 +1,9 @@
+import hashlib
+
+def calculate_sha256(filepath):
+    sha = hashlib.sha256()
+    with open(filepath, 'rb') as f:
+        while chunk := f.read(8192):
+            sha.update(chunk)
+    return sha.hexdigest()
 
