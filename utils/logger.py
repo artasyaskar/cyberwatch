390454c9555481172079c3d471ca23cb9be4de6d
Artas Yaskar <artasyaskar@gmail.com>
2025-06-07 02:32:16 +0500

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,1 +1,5 @@
+from datetime import datetime
 
+def log_threat(threat):
+    ts = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"[{ts}] THREAT DETECTED: {threat['threat']} on line {threat['line']}: {threat['match']}")
