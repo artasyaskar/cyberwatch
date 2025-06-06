29cc21deab09c4ad05196c5e3ed0d856baf038b5
Artas Yaskar <artasyaskar@gmail.com>
2025-06-07 02:29:38 +0500

    Update scheduler.py

--- core/scheduler.py
+++ core/scheduler.py
@@ -1,1 +1,16 @@
+import schedule
+import threading
+import time
 
+class LogScheduler:
+    def __init__(self, func, interval=10):
+        self.job = schedule.every(interval).seconds.do(func)
+
+    def start(self):
+        def run_scheduler():
+            while True:
+                schedule.run_pending()
+                time.sleep(1)
+        thread = threading.Thread(target=run_scheduler)
+        thread.daemon = True
+        thread.start()
